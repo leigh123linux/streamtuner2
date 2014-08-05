0de007412bcd1d2fb7dfee7c4e84175f219ec38c
mario <mario>
2014-08-05 02:42:23 +0000

    Optional homepage querying, stripping musicgoal stream_urls.

--- channels/dirble.py
+++ channels/dirble.py
@@ -2,7 +2,7 @@
 # api: streamtuner2
 # title: Dirble
 # description: New open radio station directory.
-# version: 0.1
+# version: 0.2
 # type: channel
 # category: radio
 # priority: optional
@@ -13,6 +13,9 @@
 # However useful station information (homepage, etc.) only
 # with extraneous requests. So just for testing as of now.
 #
+# Uh, and API is appearently becoming for-pay (two days
+# after writing this plugin;). So ST2 users may have to
+# request their own Dirble.com key probably.
 #
 
 
@@ -47,6 +50,7 @@ class dirble (ChannelPlugin):
     base = "http://api.dirble.com/v1/%s/apikey/%s/"
     cid = "84be582567ff418c9ba94d90d075d7fee178ad60"
 
+
     # Retrieve cat list and map
     def update_categories(self):
         self.categories = []
@@ -57,23 +61,28 @@ class dirble (ChannelPlugin):
             # Request subcats
             sub = []
             self.categories.append(sub)
-            for subrow in self.api("childCategories", ["primaryid", str(row["id"])]):
+            for subrow in self.api("childCategories", "primaryid", row["id"]):
                 sub.append(subrow["name"])
                 self.catmap[subrow["name"]] = subrow["id"]
+
 
     # Just copy over stream URLs and station titles
     def update_streams(self, cat, search=None):
     
         if cat:
             id = self.catmap.get(cat, 0);
-            data = self.api("stations", ["id", str(id)])
+            data = self.api("stations", "id", id)
         elif search:
-            data = self.api("search", ["search", search])
+            data = self.api("search", "search", search)
         else:
             pass
 
         r = []
         for e in data:
+            # skip musicgoal (resolve to just a blocking teaser)
+            if e["streamurl"].find("musicgoal") > 0:
+                continue
+            # append dict after renaming fields
             r.append(dict(
                 id = e["id"],
                 genre = str(cat),
@@ -87,19 +96,25 @@ class dirble (ChannelPlugin):
             ))
         return r
 
+
     # Request homepage for stations, else try to deduce Dirble page
     def get_homepage(self, id, name):
         if conf.dirble_fetch_homepage:
-            return self.api("station", ["id", str(id)])["website"]
+            return self.api("station", "id", id)["website"]
         else:
             name = re.sub("[^\w\s]+", "", name)
             name = re.sub("\s", "-", name)
             return "http://dirble.com/station/" + name.lower();
 
-    # Patch together
-    def api(self, method, params=[]):
-        j = http.get((self.base % (method, self.cid)) + "/".join([str(e) for e in params]))
-        r = json.loads(j);
+
+    # Patch API url together, send request, decode JSON and whathaveyou
+    def api(self, *params):
+        method = params[0]
+        j = http.get((self.base % (method, self.cid)) + "/".join([str(e) for e in params[1:]]))
+        try:
+            r = json.loads(j);
+        except:
+            r = []
         return r
 
 
