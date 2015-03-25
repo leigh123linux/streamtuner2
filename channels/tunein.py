6893fb14521b682e8816acddff6fae33aa5afe56
mario <mario>
2015-03-25 21:53:41 +0000

    Properly query "local" category.

--- channels/tunein.py
+++ channels/tunein.py
@@ -42,13 +42,13 @@ class tunein (ChannelPlugin):
 
     categories = ["local", "60's", "70's", "80's", "90's", "Adult Contemporary", "Alternative Rock", "Ambient", "Bluegrass", "Blues", "Bollywood", "Children's Music", "Christmas", "Classic Hits", "Classic Rock", "Classical", "College Radio", "Country", "Decades", "Disco", "Easy Listening", "Eclectic", "Electronic", "Folk", "Hip Hop", "Indie", "Internet Only", "Jazz", "Live Music", "Oldies", "Polka", "Reggae", "Reggaeton", "Religious", "Rock", "Salsa", "Soul and R&B", "Spanish Music", "Specialty", "Tango", "Top 40/Pop", "World"]
     config = []
-    catmap = {"local": "local", "60's": "g407", "Live Music": "g2778", "Children's Music": "c530749", "Polka": "g84", "Tango": "g3149", "Top 40/Pop": "c57943", "90's": "g2677", "Eclectic": "g78", "Decades": "c481372", "Christmas": "g375", "Reggae": "g85", "Reggaeton": "g2771", "Oldies": "c57947", "Jazz": "c57944", "Specialty": "c418831", "Hip Hop": "c57942", "College Radio": "c100000047", "Salsa": "g124", "Bollywood": "g2762", "70's": "g92", "Country": "c57940", "Classic Hits": "g2755", "Internet Only": "c417833", "Disco": "g385", "Rock": "c57951", "Soul and R&B": "c1367173", "Blues": "g106", "Classic Rock": "g54", "Alternative Rock": "c57936", "Adult Contemporary": "c57935", "Classical": "c57939", "World": "c57954", "Indie": "g2748", "Religious": "c57950", "Bluegrass": "g63", "Spanish Music": "c57945", "Easy Listening": "c10635888", "Ambient": "g2804", "80's": "g42", "Electronic": "c57941", "Folk": "g79"}
+    catmap = {"60's": "g407", "Live Music": "g2778", "Children's Music": "c530749", "Polka": "g84", "Tango": "g3149", "Top 40/Pop": "c57943", "90's": "g2677", "Eclectic": "g78", "Decades": "c481372", "Christmas": "g375", "Reggae": "g85", "Reggaeton": "g2771", "Oldies": "c57947", "Jazz": "c57944", "Specialty": "c418831", "Hip Hop": "c57942", "College Radio": "c100000047", "Salsa": "g124", "Bollywood": "g2762", "70's": "g92", "Country": "c57940", "Classic Hits": "g2755", "Internet Only": "c417833", "Disco": "g385", "Rock": "c57951", "Soul and R&B": "c1367173", "Blues": "g106", "Classic Rock": "g54", "Alternative Rock": "c57936", "Adult Contemporary": "c57935", "Classical": "c57939", "World": "c57954", "Indie": "g2748", "Religious": "c57950", "Bluegrass": "g63", "Spanish Music": "c57945", "Easy Listening": "c10635888", "Ambient": "g2804", "80's": "g42", "Electronic": "c57941", "Folk": "g79"}
 
 
     # Retrieve cat list and map
     def update_categories(self):
         self.categories = ["local"]
-        self.catmap = {"local": "local"}
+        self.catmap = {}
         # Only music for now
         for row in self.api("Browse.ashx?c=music"):
             self.categories.append(row["text"])
@@ -57,8 +57,14 @@ class tunein (ChannelPlugin):
 
     # Just copy over stream URLs and station titles
     def update_streams(self, cat, search=None):
-        r = []    
-        for row in self.api("Browse.ashx?id=" + self.catmap[cat]):
+        r = []
+        # catmap only set for genres, not for category groups like "local"    
+        if cat in self.catmap and cat != "local":
+            attr = "id=" + self.catmap[cat]
+        else:
+            attr = "c=" + cat
+        # fetch
+        for row in self.api("Browse.ashx?" + attr):
           __print__(row)
           if "URL" in row and "bitrate" in row and "subtext" in row:
             r.append({
