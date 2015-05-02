f15d234d93ea0cb7320c0106092e1535dcf55087
mario <mario>
2015-05-02 05:39:44 +0000

    More specific log.HTTP messages (GET vs POST)

--- ahttp.py
+++ ahttp.py
@@ -54,7 +54,6 @@ session.headers.update({
 #  Well, it says "get", but it actually does POST and AJAXish GET requests too.
 #
 def get(url, params={}, referer="", post=0, ajax=0, binary=0, feedback=None, content=True, verify=False):
-    log.HTTP("GET", url, params )
 
     # statusbar info
     progress_feedback(url)
@@ -72,8 +71,10 @@ def get(url, params={}, referer="", post=0, ajax=0, binary=0, feedback=None, con
     
     # read
     if post:
+        log.HTTP("POST", url, params)
         r = session.post(url, params=params, headers=headers, timeout=7.5)
     else:    
+        log.HTTP("GET"+(" AJAX" if ajax else ""), url, params )
         r = session.get(url, params=params, headers=headers, verify=verify, timeout=9.75)
 
     log.HTTP(">>>", r.request.headers );
