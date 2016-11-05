95f6092cf824d6bcdd3a2c2a917a855b1a2dce1f
mario <mario>
2016-11-05 23:55:46 +0000

    Reciva: add search function

--- contrib/reciva.py
+++ contrib/reciva.py
@@ -3,7 +3,7 @@
 # title: Reciva
 # url: https://radios.reciva.com/stations/genre/34?&start=100&count=50
 # description: Home internet radio app and diverse station database.
-# version: 0.3
+# version: 0.4
 # type: channel
 # category: radio
 # config: -
@@ -39,7 +39,7 @@ class reciva (ChannelPlugin):
     # module attributes
     module = "reciva"
     listformat = "pls"
-    has_search = False
+    has_search = True
     categories = ['60s', '70s', '80s', '90s', 'Adult', ['Adult Contemporary'], 'Alternative', 'Ambient', 'Bluegrass', 'Blues', 'Bollywood', 'Christian', ['Christian Contemporary'], 'Classic Rock', 'Classical', 'College', 'Comedy', 'Contemporary', 'Country', 'Dance', 'Discussion', 'Easy', 'Electronica', 'Experimental', 'Folk', 'Gospel', 'Greek', 'Hip Hop', 'Indian', 'Indie', ['Indie Rock'], 'Jazz', 'Jungle', 'Kids', 'Latin Hits', 'New Age', 'News', ['News Talk', 'News Updates'], 'Oldies', 'Pop', 'Public', 'Punk', 'Rap', 'Reggae', 'Religious', 'Rock', 'Short Wave Radio', 'Soft Rock', 'Spanish', 'Sports', 'Talk', 'Top 40', 'Unknown', 'Varied', 'World', ['World Africa', 'World Asia', 'World Caribbean', 'World Europe', 'World Mediterranean', 'World Middle East', 'World Tropical']]
     catmap = { 'classical': '14', 'dance': '18', 'bluegrass': '52', 'contemporary': '16', 'pop': '34', 'spanish': '66', 'college': '15', 'rap': '38', 'ambient': '69', 'talk': '43', 'alternative': '9', 'religious': '39', 'blues': '10', 'folk': '23', 'classic rock': '13', '90s': '7', 'adult contemporary': '8', 'oldies': '33', 'indie rock': '54', 'electronica': '21', 'unknown': '45', 'discussion': '19', 'news talk': '31', 'world mediterranean': '55', 'sports': '42', 'new age': '51', 'indie': '27', 'indian': '65', 'easy': '20', '80s': '6', 'world africa': '67', 'comedy': '62', 'public': '35', 'jungle': '72', 'reggae': '48', 'world middle east': '50', 'christian': '11', 'world caribbean': '68', '60s': '58', 'world europe': '56', 'jazz': '28', '70s': '5', 'soft rock': '41', 'top 40': '44', 'adult': '57', 'news': '30', 'bollywood': '60', 'world tropical': '53', 'latin hits': '29', 'varied': '46', 'christian contemporary': '12', 'kids': '59', 'short wave radio': '73', 'world': '49', 'world asia': '47', 'country': '17', 'news updates': '32', 'punk': '36', 'greek': '25', 'hip hop': '26', 'rock': '40', 'gospel': '24', 'experimental': '22' }
     titles = dict( genre="Genre", title="Station", playing="Location", bitrate="Bitrate", listeners=False )
@@ -62,9 +62,12 @@ class reciva (ChannelPlugin):
 
 
     # fetchy fetch
-    def update_streams(self, cat):
+    def update_streams(self, cat, search=None):
         entries = []
-        html = ahttp.get(self.base_url % (self.catmap[cat.lower()], conf.max_streams))
+        if cat:
+            html = ahttp.get(self.base_url % (self.catmap[cat.lower()], conf.max_streams))
+        else: # search
+            html = ahttp.get("https://radios.reciva.com/stations/search?q=%s&categories=&codec=&min_bitrate=&max_bitrate=&working=true&count=%s" % (search, conf.max_streams))
         if not html:
             log.ERR("No results from http://radios.reciva.com/ server. Their category browsing sometimes breaks. We're not using the search function as that would strain their server too much. You might try adding login credentials to `.netrc` - albeit that rarely helps.", html)
             return []
