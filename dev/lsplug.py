c9bce3c811fd4f901e494eec2a2b0cbf4ad6a7e1
mario <mario>
2016-12-03 21:24:43 +0000

    lsplugins: export {png} to help/img/

--- dev/lsplug.py
+++ dev/lsplug.py
@@ -1,13 +1,22 @@
 # description: list available plugins for wiki
 
 from config import *
+from base64 import b64decode
 import pluginconf
 pluginconf.module_base = "config"
 pluginconf.plugin_base = ["channels", "contrib"]#, conf.share+"/channels", conf.dir+"/plugins"]
 
 for name,e in pluginconf.all_plugin_meta().items():
+
+    # print table
     if "title" in e:
         try:
             print "| [{title}]({url}) | **{version}** | {type} | {category} | *{priority}* | {description} |".format(**e)
         except Exception, e:
             print "ERROR*** ", name, e
+    
+    # extract icon
+    if False and "png" in e:
+        with open("help/img/%s_%s.png" % (e["type"], name), "wb") as f:
+            f.write(b64decode(e["png"]))
+    
