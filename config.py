f1a796d2b100df80b4b3be03eb0ce13e2f14d046
mario <mario>
2017-11-17 13:20:10 +0000

    Change default streamripper user agent to 'iTunes 12.5/Gecko/SR'

--- config.py
+++ config.py
@@ -123,7 +123,7 @@ class ConfigDict(dict):
            "url/http": self.find_player(typ="browser"),
         }
         self.record = {
-           "audio/*": self.find_player(typ="xterm", append=' -e "streamripper %srv" -u "NSPlayer/10.0.0.3802" '),   # -d /home/***USERNAME***/Musik
+           "audio/*": self.find_player(typ="xterm", append=' -e "streamripper %srv -u ''iTunes/12.5.5 (Linux; X11; Gecko rv:57) SR/1.64''" '),   # -d /home/***USERNAME***/Musik
            "video/youtube": self.find_player(typ="video", append=' $("youtube-dl %srv")'),
         }
         self.specbuttons = {
